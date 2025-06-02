0f8966d0b40f56b3b7b747f31e36820effa528f4
Vedant Patil <vedantpatil1709@gmail.com>
2025-06-02 10:27:21 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,77 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "./globals.css";
+import Layout, { Content, Header } from "antd/es/layout/layout";
+import Breadcrumb from "antd/es/breadcrumb";
+import Menu from "antd/es/menu";
+import { Button } from "antd";
+import Link from "next/link";
+import Footer from "@/my-components/FooterComponent";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "HR Consultancy",
+  description: "HR Consultancy - Your Partner in Human Resources",
+};
+
+const items = [
+  { label: "Home", key: "/home" },
+  { label: "About Us", key: "/about-us" },
+  { label: "Services", key: "/services" },
+  { label: "Contact", key: "/contact" },
+];
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body style={{
+        overflow:"visible",
+      }}>
+        <AntdRegistry>
+          <Layout>
+            <Header
+              style={{
+                background: "#fff",
+                position: "sticky",
+                top: 0,
+                zIndex: 1,
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {/* <div className="demo-logo" /> */}
+              <Menu
+                theme="light"
+                mode="horizontal"
+                defaultSelectedKeys={["2"]}
+                items={items.map((item) => ({
+                  key: item.key,
+                  label: <Link href={item.key}>{item.label}</Link>,
+                }))}
+                className="flex justify-center flex-1"
+              />
+              <Button type="primary">Get started</Button>
+            </Header>
+            <Content className="height-full">{children}</Content>
+            <Footer/>
+          </Layout>
+        </AntdRegistry>
+      </body>
+    </html>
+  );
+}
---
+++ src/my-components/ServiceCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Card, Typography } from 'antd';
+import Link from 'next/link';
+const { Title, Paragraph } = Typography;
+
+interface ServiceCardProps {
+  title: string;
+  description: string[];
+  icon: React.ReactNode;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({ title, description, icon }) => {
+  return (
+    <Link href="/services\" className="block">
+      <Card
+        variant='outlined'
+        className="service-card h-full"
+        hoverable
+      >
+        <div className="text-center mb-4 text-[#1890ff]">
+          {icon}
+        </div>
+        <Title level={4} className="text-center mb-4">
+          {title}
+        </Title>
+        <ul className="pl-5">
+          {description.map((item, index) => (
+            <li key={index} className="mb-2">
+              <Paragraph>{item}</Paragraph>
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </Link>
+  );
+};
+
+export default ServiceCard;
